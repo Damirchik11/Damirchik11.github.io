dc71c090579a22568e3fb8d464fc483bfc66d0ce
Damirchik11 <114121743+Damirchik11@users.noreply.github.com>
2023-10-26 22:43:07 -0700

    Added react.png, edited other files

--- Script.js
+++
@@ -1,8 +0,0 @@
-function toggleMobileNav() {
-    var nav = document.getElementById("mobile-nav");
-    if (nav.style.display === "block") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "block";
-    }
-}
